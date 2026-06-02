635c61d9a258d17239b4d2ecba7cb423eb8a3b0f
shreyashrangari08-gif <shreyashrangari08@gmail.com>
2026-06-02 21:05:07 +0530

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,11 @@
+const Order = require('../models/Order');
+
+exports.createOrder = async (req, res) => {
+    try {
+        const newOrder = new Order(req.body);
+        await newOrder.save();
+        res.status(201).json({ message: "Order placed successfully!" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
