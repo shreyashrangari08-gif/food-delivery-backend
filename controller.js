5bb2f82adfda5026fdf3bcbdcb5bacf7453f2d0b
shreyashrangari08-gif <shreyashrangari08@gmail.com>
2026-06-02 21:45:12 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,11 +1,11 @@
-const Order = require('../models/Order');
+const Order = require('./modelsorder'); // Path fix kiya
 
 exports.createOrder = async (req, res) => {
-    try {
-        const newOrder = new Order(req.body);
-        await newOrder.save();
-        res.status(201).json({ message: "Order placed successfully!" });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const newOrder = new Order(req.body);
+    await newOrder.save();
+    res.status(201).json({ message: "Order placed successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
